919f8e7332e24d89329f4001cccaf2ad05308db8
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2023-07-25 22:01:59 +0900

    Fix: cookie에 저장되는 값 수정

--- src/pages/Redirection.js
+++ src/pages/Redirection.js
@@ -4,16 +4,12 @@ import axios from "axios";
 
 const Redirection = () => {
 	const code = new URL(window.location.toString()).searchParams.get("code");
-	console.log("code", code);
 	const navigate = useNavigate();
 
 	useEffect(() => {
 		console.log(process.env.REACT_APP_SERVER);
 		axios.post(`${process.env.REACT_APP_SERVER}/api/user/kakao/login?code=${code}`).then((r) => {
-			console.log("r.data", r.data);
-			console.log("r.header", r.headers);
-			document.cookie = `accessToken=${r.headers.accesstoken}; path=/;`;
-			// 토큰을 받아서 localStorage/쿠키에 같은 곳에 저장하는 코드를 여기에 쓴다.
+			document.cookie = `accessToken=${r.headers.authorization}; path=/;`;
 			navigate("/");
 		});
 	}, []);
